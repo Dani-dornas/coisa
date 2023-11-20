5d1e46c09ec833ae3b2bb9c99e507199e1198240
Dani-dornas <lauby.dd@gmail.com>
2023-11-19 21:31:26 -0300

    backup

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,6 +3,9 @@ import App from "./app/App";
 import React from "react";
 import { AuthProvider } from "./routes/context/auth/Authprovider";
 import { BrowserRouter } from "react-router-dom";
+import Header from "./routes/components/Header";
+import Footer from "./routes/components/Footer";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -10,9 +13,13 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <AuthProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <GoogleOAuthProvider clientId="696482695372-ko2n7vnkebs0hr9gdujukmdh7ubpr758.apps.googleusercontent.com">
+        <BrowserRouter>
+          <Header />
+          <App />
+          <Footer />
+        </BrowserRouter>
+      </GoogleOAuthProvider>
     </AuthProvider>
   </React.StrictMode>
 );
--- frontend/src/routes/home/Home.tsx
+++ frontend/src/routes/home/Home.tsx
@@ -1,86 +1,14 @@
 import "./home.css";
-import homebikes from "./img/homebikes.png";
-import { Row, Col, Container } from "react-bootstrap";
-import Header from "../components/Header";
-import { Link, useParams } from "react-router-dom";
-import Footer from "../components/Footer";
-import { useEffect, useState } from "react";
-import axios from "axios";
-import { BikeProps } from "../../types";
+import { Container } from "react-bootstrap";
+import HomeBikeAds from "./components/HomeBikeAds";
+import HomeHeader from "./components/HomeHeader";
 
 function Home() {
-  const [bikes, setBikes] = useState<BikeProps[]>([]);
-
-  const getBikes = async () => {
-    try {
-      const response = await axios.get(`http://localhost:3001/bike/`);
-      const data = response.data;
-      setBikes(data);
-      console.log("Teste1", data)
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  useEffect(() => {
-    getBikes();
-  }, [])
-
   return (
-    <div>
-      <Header />
-      <main>
-        <Container>
-          <Row id="headerhome">
-            <Col>
-              <Row id="alignrow">
-                <h1>
-                  <b>O MELHOR AGREGADOR PARA ALUGUEL DE BICICLETAS</b>
-                </h1>
-                <h3>
-                  <b>Sua melhor escolha para pedalar</b>
-                </h3>
-                <Link className="catalog-button" to="/catalog">
-                  Acesse o Catálogo
-                </Link>
-              </Row>
-            </Col>
-            <Col>
-              <img src={homebikes} alt="home page bikes" />
-            </Col>
-          </Row>
-
-          <Row id="mainhome1">
-            <h2>Escolha a bicicleta que faz mais o seu estilo</h2>
-            <Row>
-              {bikes.length === 0 ? (<p>Carregando...</p>) : (
-                bikes?.map((bike) => (
-                  <div className="home-bike-card" key={bike.id}>
-                    <div className="adjust">
-                      {bike.photos[0] ? (
-                        <img
-                          src={`http://localhost:3001/photo/public/${bike.photos[0].filename}`}
-                          alt="bike"
-                          className="home-bike-photo"
-                        />
-                      ) : (
-                        <p>Foto não disponível</p>
-                      )}
-                    </div>
-                    <div className="adjust">
-                      <Link className="home-bike-button" to="/bike">
-                        Confirma
-                      </Link>
-                    </div>
-                  </div>
-                ))
-              )}
-            </Row>
-          </Row>
-        </Container>
-      </main>
-      <Footer />
-    </div>
+    <Container id="home">
+      <HomeHeader />
+      <HomeBikeAds />
+    </Container>
   );
 }
 
